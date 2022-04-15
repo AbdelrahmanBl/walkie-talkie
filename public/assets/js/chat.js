fae7ed387a7deb13e5971058741493b68dbe3637
AbdelrahmanBl <abdelrahmangamal990@gmail.com>
2022-04-15 17:08:16 +0200

    polyfill

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -3,20 +3,17 @@ let socket = io()
 // let counter = 0
 
 socket.on('audio' , async (buffer) => {
-    let blob = new Blob([buffer], {type: 'audio/ogg'})
+    let blob = await new Blob([buffer], {type: 'audio/ogg'})
     // console.log(blob);
-    var url = URL.createObjectURL(blob);
+    var url = await URL.createObjectURL(blob);
     
     // let audioMessage = new Audio(url)
     audioBefore.play()
     clientAudio.src = url
-    alert('set')
     clientAudio.play()
     clientAudio.onended = () => {
-        alert('end')
         audioAfter.play()
     }
-    alert('end-end')
 })
 
 socket.on('canRecord', status => {
